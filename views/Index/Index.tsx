855efb4f54d91fc99977c1dbc0e7498c7403df99
msobalvarro <mssrsobalvarro1997@gmail.com>
2019-08-24 00:40:06 -0600

    view Index

--- views/Index/Index.tsx
+++ views/Index/Index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Image, Text, SafeAreaView} from 'react-native'
+import { Image, Text, SafeAreaView, TouchableOpacity, View} from 'react-native'
 import { Index as styles } from '../styles'
 /**
  * Selection view of **Hostelers** and **distributors**
@@ -9,7 +9,16 @@ class Index extends Component {
         return (
             <SafeAreaView style={styles.main}>
                 <Image style={styles.imageLogo} resizeMode="contain" source={require('../../assets/logo/logo.png')} />
-                <Text>Test</Text>
+                
+                <View style={styles.containerButtons}>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.textButton}>Soy Hostelero</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.textButton}>Soy Distribuidor</Text>
+                    </TouchableOpacity>                    
+                </View>
+
             </SafeAreaView>
         )
     }
--- views/styles.tsx
+++ views/styles.tsx
@@ -1,19 +1,39 @@
-import { StyleSheet, ViewStyle, ImageStyle, Platform } from 'react-native'
-import { RFPercentage } from 'react-native-responsive-fontsize'
+import { StyleSheet, ViewStyle, ImageStyle, Platform, TextStyle } from 'react-native'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import { Dimensions } from 'react-native'
 
 const { create } = StyleSheet
 
+/**Dimension screen app */
+const { width, height } = Dimensions.get("screen")
+
+/**Return size **%** width screen app */
+const screenWidth = (e:number): number => {
+    return Number(width * Number(`0.${e}`))
+}
+
+/**Return size **%** height screen app */
+const screenHeight = (e:number): number => {
+    return Number(height * Number(`0.${e}`))
+}
+
 interface DefaultStylesIndex {
+    buttons?: ViewStyle,
     main: ViewStyle,
     imageLogo: ImageStyle,
-    buttons?: ViewStyle
+    containerButtons?: ViewStyle,
+    textButton?: TextStyle
 }
 
 /**General styles screen Index view (main screen) */
 export const Index = create<DefaultStylesIndex>({
     /**Style main container */
     main: {
-        paddingTop: (Platform.OS === 'android') ? 50 : 0
+        paddingTop: (Platform.OS === 'android') ? 50 : 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width,
+        height,
     },
 
     /**Styles logo image */
@@ -22,5 +42,31 @@ export const Index = create<DefaultStylesIndex>({
         borderWidth: 2,
         height: RFPercentage(40),
         width: RFPercentage(40),
+        resizeMode: 'cover',
+        borderRadius: 20,
+    },
+
+    containerButtons: {
+        alignItems: 'center',
+        flexDirection: 'column',
+        marginTop: 25,
+        width: '100%',
+    },
+
+    /**Buttons style */
+    buttons: {
+        backgroundColor: '#34495e',
+        borderRadius: 3,
+        elevation: 5,
+        marginVertical: 10,
+        padding: RFValue(8),
+        width: '70%',
+    },
+
+    /**Text buttons styles */
+    textButton: {
+        color: '#FFF',
+        fontSize: RFValue(24),        
+        textAlign: 'center',
     }
 })
